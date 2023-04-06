78e40b249ecea425e1178369798b8736b70f8b7b
Angus Foreman <59096860+angusforeman@users.noreply.github.com>
2023-04-06 14:26:16 +0000

    first commit

---
+++ setupenv.sh
@@ -0,0 +1,9 @@
+# download and set up bin 
+# From https://go.dev/doc/install 
+sudo rm -rf /usr/local/go #remove any previous GO implementations (may break new)
+sudo mkdir /usr/local/go 
+mkdir /tmp
+wget --directory-prefix=./tmp https://dl.google.com/go/go1.20.3.linux-amd64.tar.gz
+sudo tar -C /usr/local/ -xzf ./tmp/go1.20.3.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin # Add go bin to the path
+go version # if sucesfull will show the installed version 
